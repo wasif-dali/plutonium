a3985b4f91004f8751e95b670445697bca4ff830
sabiha <you@example.com>
2022-08-25 03:18:39 +0530

    all update

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const authorSchema = new mongoose.Schema( {
-    author_id: String,
+    
     author_name: String,
     age:Number,
-    address:String
+    address:String,
+    rating:Number
 
 }, { timestamps: true });
 
