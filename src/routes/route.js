a977590c5f454ac6d0fe96805717396e39a5a2ba
sabiha <you@example.com>
2022-08-26 01:07:48 +0530

    all update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment')
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
+//const UserController= require("../controllers/userController")
+//const BookController= require("../controllers/bookController")
+//const commonMW = require ("../middlewares/commonMiddlewares")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
+router.get("/Info", function (req, res) {
+    let now = moment();
+    const ipAddress = req.ip
+    const  f= req.originalUrl
+    console.log(now.format('DD-MM-YYYY,HH:MM:ss'),",", ipAddress,",",f);
 })
 
 
 
 
-router.post("/createBook", BookController.createBook  )
 
 
 
@@ -48,7 +51,7 @@ router.post("/createBook", BookController.createBook  )
 
 
 
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+//router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
 
 
 
